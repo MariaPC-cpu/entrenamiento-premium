aeb1f8f252704989ad30af3a980cd410c0e515a4
MariaPC-cpu <specialpax@gmail.com>
2026-02-19 22:14:03 +0100

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Entrenamiento Premium",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
